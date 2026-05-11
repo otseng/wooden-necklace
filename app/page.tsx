983e90d538b7b0751dac7aade8b5e3d199692f79
Oliver Tseng <otseng@yahoo.com>
2026-05-10 22:22:06 -0400

    add content

--- app/page.tsx
+++ app/page.tsx
@@ -7,15 +7,306 @@ export default function Page() {
        Wooden Chain Necklace
       </h1>
       <p className="mb-4">
-        {`I'm a Vim enthusiast and tab advocate, finding unmatched efficiency in
-        Vim's keystroke commands and tabs' flexibility for personal viewing
-        preferences. This extends to my support for static typing, where its
-        early error detection ensures cleaner code, and my preference for dark
-        mode, which eases long coding sessions by reducing eye strain.`}
+        {`
+        1) Original ¾ x ¾
+        This is cut with table saw and planed with thickness planer to exactly
+        ¾. I use hardwoods, mostly Cherry, Walnut, and Oak – but have made some
+        chains in Teak, Mahogany, Purple Heart, Ebony, Bamboo, and others.
+        Note: It takes a minimum of 24” to make a necklace. Since that can be a
+        bit awkward to work with, I often cut it to 12”. A little extra length
+        is good too.
+        `}
+      </p>
+      <div className="my-8">
+        <img
+          src="/image001.png"
+          className="rounded-lg shadow-md mx-auto"
+        />
+      </div>
+
+       <p className="mt-4 mb-4">
+        {`
+        2) Notch
+        It is important to cut very exactly (dimensions 7mm horizontal, 5mm
+        vertical, 7mm horizontal )
+        This jig set up to and is NEVER moved.
+        `}
+      </p>
+      <div className="flex gap-4">
+        <img src="/image003.png" alt="First" className="w-1/2"/>
+        <img src="/image005.png" alt="Second" className="w-1/2"/>
+      </div>
+
+      <p className="mt-4 mb-4">
+        {`
+        3) Mark
+        Mark every ¾” (19mm)
+        `}
+      </p>
+      <div className="my-8">
+        <img
+          src="/image009.png"
+          className="rounded-lg shadow-md mx-auto"
+        />
+      </div>
+
+
+      <p className="mt-4 mb-4">
+        {`
+        4) Drill
+Use a drill press and 1/8 (3mm) drill bit to drill at each mark, then
+flip the piece 90 degrees and drill to match each hole. The bit will
+self-center in the V
+        `}
+      </p>
+      <div className="my-8">
+        <img
+          src="/image011.png"
+          className="rounded-lg shadow-md mx-auto"
+        />
+      </div>
+
+
+      <p className="mt-4 mb-4">
+        {`
+        5) Router
+Router bit 1/8” (3mm). Using jig on drill press, router a slot between every other two sets of holes. Do both sides then rotate the piece 90 degrees and router slots alternate to the first set. I made a jig to with a slot and use a C-clamp to hold it in place on the drill press table.
+        `}
+      </p>
+      <div className="my-8">
+        <img
+          src="/image013.png"
+          className="rounded-lg shadow-md mx-auto"
+        />
+      </div>
+
+
+      <p className="mt-4 mb-4">
+        {`
+        7) Cut links apart
+The very BEST saw to use is Megacast curved jewelers saw frame $27.88 from Amazon.com
+It is the ONLY one I will use, after trying many. Also, rather than using coping saw blades, I use a 6” piece cut from a 1/8” bandsaw blade that is sturdier and cuts better.
+
+        `}
+      </p>
+      <div className="my-8">
+        <img
+          src="/image015.png"
+          className="rounded-lg shadow-md mx-auto"
+        />
+      </div>
+
+
+      <p className="mt-4 mb-4">
+        {`
+        I made the jig to hold the piece wile cutting.  Cut exactly halfway in each slot.
+        `}
+      </p>
+      <div className="flex gap-4">
+        <img src="/image017.png" alt="First" className="w-1/2"/>
+        <img src="/image019.png" alt="Second" className="w-1/2"/>
+      </div>
+
+
+      <p className="mt-4 mb-4">
+        {`
+        8) Cut off the scrap ends (photo using nippers)
+This can be done many ways. The quickest and best for me is to use nippers
+
+        `}
+      </p>
+      <div className="my-8">
+        <img
+          src="/image021.png"
+          className="rounded-lg shadow-md mx-auto"
+        />
+      </div>
+
+
+      <p className="mt-4 mb-4">
+        {`
+        9) Rough sand
+Use small disc in drill press with fine grit paper.
+This step just removes the high place – gentle touch, don’t overdo
+        `}
+      </p>
+      <div className="my-8">
+        <img
+          src="/image023.png"
+          className="rounded-lg shadow-md mx-auto"
+        />
+      </div>
+
+
+      <p className="mt-4 mb-4">
+        {`
+        10) Outside shape (photo of sander mount and process)
+Use a 1x30 belt sander (Amazon or Harbor Freight $70 to $100 range), and made a wooden stand to hold the sander to run horizontally.
+<br />
+Mostly just rounding corners – don’t take off much
+
+        `}
+      </p>
+      <div className="my-8">
+        <img
+          src="/image025.png"
+          className="rounded-lg shadow-md mx-auto"
+        />
+      </div>                                    
+      <div className="my-8">
+        <img
+          src="/image027.png"
+          className="rounded-lg shadow-md mx-auto"
+        />
+      </div>   
+
+      <p className="mt-4 mb-4">
+        {`
+        11) Inside shape (photo of Dremel and bit plus process)
+One of the Dremel carving bits does a nice job shaping the inside of each link
+        `}
+      </p>
+      <div className="my-8">
+        <img
+          src="/image029.png"
+          className="rounded-lg shadow-md mx-auto"
+        />
+      </div>                                    
+      <div className="my-8">
+        <img
+          src="/image031.png"
+          className="rounded-lg shadow-md mx-auto"
+        />
+      </div>   
+
+      <p className="mt-4 mb-4">
+        {`
+        12) Inside sand
+Sanding drum and sleeve 1/8”
+Notice that I use a small ‘Dremel-like’ rotary tool from Harbor Freight (less than $10) and 60 or 80 grit sleeve.
+
+        `}
+      </p>
+      <div className="my-8">
+        <img
+          src="/image033.png"
+          className="rounded-lg shadow-md mx-auto"
+        />
+      </div>                                    
+
+
+      <p className="mt-4 mb-4">
+        {`
+        13) Outside finish shape
+The little sander is from Harbor Freight and I made a bracket to hold it running horizontally. It is important to keep the link level as it is being shaped. Using 120 grit belts.
+
+
+        `}
+      </p>
+      <div className="my-8">
+        <img
+          src="/image035.png"
+          className="rounded-lg shadow-md mx-auto"
+        />
+      </div>     
+
+
+      <p className="mt-4 mb-4">
+        {`
+        Rounding the sharp edges of each link makes the chain more comfortable to wear, and more attractive. I use a ½ inch drum and 120 or finer sleeve with the little rotary tool.
+
+        `}
+      </p>
+      <div className="my-8">
+        <img
+          src="/image037.png"
+          className="rounded-lg shadow-md mx-auto"
+        />
+      </div>     
+
+
+      <p className="mt-4 mb-4">
+        {`
+        14) Surface sand - 
+I make sanding sticks with thin pieces of scrap wood with 120 grit and finer sandpaper glued to one or both sides.
+
+        `}
       </p>
       {/* <div className="my-8">
-        <BlogPosts />
-      </div> */}
+        <img
+          src="/image001.png"
+          className="rounded-lg shadow-md mx-auto"
+        />
+      </div>      */}
+
+
+      <p className="mt-4 mb-4">
+        {`
+        15) Inside finish - 
+The 1/8” sanding drum with 120 or finer sleeve – it is important to smooth the length as well as shape the rounded ends
+
+        `}
+      </p>
+      {/* <div className="my-8">
+        <img
+          src="/image001.png"
+          className="rounded-lg shadow-md mx-auto"
+        />
+      </div>      */}
+
+
+      <p className="mt-4 mb-4">
+        {`
+        17) Join ends - 
+Break one of the end links with the grain and use it to join the two ends. A good wood glue like Gorilla wood glue will make the link stronger than it was originally. A small C-clamp works nicely, particularly with the adapter to eliminate twisting.
+
+        `}
+      </p>
+      {/* <div className="my-8">
+        <img
+          src="/image001.png"
+          className="rounded-lg shadow-md mx-auto"
+        />
+      </div>      */}
+
+
+      <p className="mt-4 mb-4">
+        {`
+        18) Final finish sanding with fine emery cloth or sandpaper 
+        `}
+      </p>
+      {/* <div className="my-8">
+        <img
+          src="/image001.png"
+          className="rounded-lg shadow-md mx-auto"
+        />
+      </div>      */}
+
+
+      <p className="mt-4 mb-4">
+        {`
+        19) Oil if desired – linseed oil, tung oil, Watco Danish oil, or (my favorite but slow) William weir Wood wax oil (paste in a can) Photo
+        `}
+      </p>
+      <div className="my-8">
+        <img
+          src="/image041.png"
+          className="rounded-lg shadow-md mx-auto"
+        />
+      </div>    
+
+      <p className="mt-4 mb-4">
+        {`
+        `}
+      </p>
+      <div className="my-8">
+        <img
+          src="/image039.png"
+          className="rounded-lg shadow-md mx-auto"
+        />
+      </div>    
+
+
     </section>
   )
 }
